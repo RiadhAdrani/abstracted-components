41dc2f663e8b1c490703fbef691685662a85336b
adrani_laptop <riadh_adrani@hotmail.fr>
2021-06-08 18:11:49 +0100

    [html] added AnchorView component

---
+++ base/Anchor.js
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import PropUtil from "../attributes/PropUtil";
+
+const Anchor = (props) => {
+     const content = `{${PropUtil.getCSS(props)}}`;
+
+     // console.log(content);
+
+     const animations = `${props.animations}`;
+
+     const Comp = styled.a`
+          ${content}
+
+          :hover {
+               ${props.hover}
+          }
+
+          :focus {
+               ${props.focus}
+          }
+
+          :active {
+               ${props.active}
+          }
+
+          ::after {
+          }
+
+          ::before {
+          }
+
+          ${animations}
+     `;
+
+     // console.log(props.hover);
+
+     return (
+          <Comp
+               key={props.key}
+               className={props.className}
+               id={props.id}
+               href={props.link}
+               target={props.target}
+          >
+               {props.children}
+          </Comp>
+     );
+};
+
+export default Anchor;
