5f66764de2f03e4807e428edc42cd10935c073a2
adrani_laptop <riadh_adrani@hotmail.fr>
2021-06-20 16:05:21 +0100

    [css] added fit-content and max-content values to height, width, max/min-height/width

--- attributes/Height.js
+++ attributes/Height.js
@@ -1,15 +1,20 @@
 import PropUtil from "./PropUtil";
 
-class Height{
+class Height {
+     static Auto = "auto";
+     static Initial = "initial";
+     static Inherit = "inherit";
+     static FitContent = "fit-content";
+     static MaxContent = "max-content";
 
-    static Auto = "auto";
-    static Initial = "initial";
-    static Inherit = "inherit";
-
-    static checkProperty(prop, backup, backup2 = this.Initial){
-        return PropUtil.isLengthProperty(prop,[this.Auto,this.Inherit,this.Initial],backup,backup2);
-    }
-
+     static checkProperty(prop, backup, backup2 = this.Initial) {
+          return PropUtil.isLengthProperty(
+               prop,
+               [this.Auto, this.Inherit, this.Initial, this.FitContent, this.MaxContent],
+               backup,
+               backup2
+          );
+     }
 }
 
 export default Height;
